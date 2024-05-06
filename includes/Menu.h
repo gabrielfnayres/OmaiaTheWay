03f96fd6e45e89a3067ab4a9483059609134c047
Adriane Lopes <adrianelf04@gmail.com>
2024-05-06 16:53:04 -0300

    menu

---
+++ includes/Menu.h
@@ -0,0 +1,18 @@
+#pragma once
+#include <iostream>
+
+using namespace std;
+
+class Menu{
+public:
+void exibir();
+void setUsuario();
+Menu();
+char lerResposta();
+
+
+
+protected:
+string nome;
+
+};
