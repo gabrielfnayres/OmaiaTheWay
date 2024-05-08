a7635ebae3e74732b2d2b63a2d8cc6b8378edfe2
gaabsjrer <gabriellucenasajr@gmail.com>
2024-05-07 22:27:10 -0300

    correção dos erros de milhões

--- src/facedetect_simple.cpp
+++ src/facedetect_simple.cpp
@@ -1,4 +1,4 @@
-include "opencv2/objdetect.hpp"
+#include "opencv2/objdetect.hpp"
 #include "opencv2/highgui.hpp"
 #include "opencv2/imgproc.hpp"
 #include "opencv2/videoio.hpp"
@@ -147,8 +147,6 @@ int main( int argc, const char** argv )
     }
 
     }
-    
-
     return 0;
 }
 
