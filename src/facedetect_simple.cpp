6543a0d917147ff4d1ef3c0387c64a52dd3ee9cb
gaabsjrer <gabriellucenasajr@gmail.com>
2024-05-07 22:03:38 -0300

    besteira

--- src/facedetect_simple.cpp
+++ src/facedetect_simple.cpp
@@ -1,4 +1,4 @@
-#include "opencv2/objdetect.hpp"
+include "opencv2/objdetect.hpp"
 #include "opencv2/highgui.hpp"
 #include "opencv2/imgproc.hpp"
 #include "opencv2/videoio.hpp"
@@ -189,9 +189,7 @@ int detectAndDraw( Mat& img, CascadeClassifier& cascade, double scale, bool tryf
     Mat pipe4 = cv::imread("../data/pipe_original.png", IMREAD_UNCHANGED);
     Rect pipeRect4 = Rect(x3, y_up, pipe4.cols, pipe4.rows);
 
-    Mat flappy = cv::imread("../data/bird_sprite(1)(1).png", IMREAD_UNCHANGED);
-    
-
+    Mat flappy = cv::imread("../data/bird_sprite(1).png");
 
     c++;
     cout << endl << "c = " << c << endl;
@@ -250,18 +248,11 @@ int detectAndDraw( Mat& img, CascadeClassifier& cascade, double scale, bool tryf
     for ( size_t i = 0; i < faces.size(); i++ )
     {
         Rect r = faces[i];
-        //cout << "Rectangle - P1: " << Point(cvRound(r.x+60), cvRound(r.y+60)) << endl << endl;
-        //cout << "Rectangle - P2: " << Point(cvRound(r.x+110), cvRound(r.y+110)) << endl << endl;
-
-        //drawTransparency(smallImg, flappy, cvRound(r.x+85),cvRound(r.y+85));
-
 
         Rect fac = Rect(cvRound(r.x+(r.width/2) - 25),cvRound(r.y+ (r.height/2) - 25), 50, 50);
 
-
         if(((fac & pipeRect1).area() > 1)||((fac & pipeRect2).area() > 1)||((fac & pipeRect3).area() > 1)||((fac & pipeRect4).area() > 1))
         {
-          
             color = Scalar(0,0,255);
             
             putText	(smallImg, "GAME OVER", Point(200, 200), FONT_HERSHEY_PLAIN, 3, Scalar(255,255,255));
@@ -273,11 +264,8 @@ int detectAndDraw( Mat& img, CascadeClassifier& cascade, double scale, bool tryf
         {
             color = Scalar(255,0,0);
         }
-
-        rectangle( smallImg, Point(cvRound(r.x+60), cvRound(r.y+60)), Point(cvRound((r.x + 110)), cvRound((r.y + 110))), color, 3);
-        if(cvRound(r.x + 85) < 300 && cvRound(r.y + 85) < 300){
-          drawTransparency(smallImg, flappy, cvRound(r.x+(r.width/2) - 25),cvRound(r.y+ (r.height/2) - 25));
-        }
+        //rectangle( smallImg, Point(cvRound(r.x+60), cvRound(r.y+60)), Point(cvRound((r.x + 110)), cvRound((r.y + 110))), color, 3);
+        rectangle( smallImg, Point(cvRound(fac.x), cvRound(fac.y)), Point(cvRound((fac.x + fac.width)), cvRound((fac.y + fac.height))), color, 3);
 
     }   
    
