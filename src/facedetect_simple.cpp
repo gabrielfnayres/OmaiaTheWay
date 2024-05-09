2728032b921f3786aa155b3498215b12906a7215
gaabsjrer <gabriellucenasajr@gmail.com>
2024-05-08 22:30:00 -0300

    finalizado menu

--- src/facedetect_simple.cpp
+++ src/facedetect_simple.cpp
@@ -38,7 +38,7 @@ int pontos;
 Menu menu;
 char resp;
 string nome;
-string numeracao= " ";
+string numeracao = " ";
 string usuarioTopo;
 
 
@@ -95,67 +95,59 @@ int main( int argc, const char** argv )
         return -1;
     }
 
-    if(!capture.open(0)) // para testar com um video
-    //if(!capture.open(0)) // para testar com a webcam
+    if(!capture.open(0)) 
     {
         cout << "Capture from camera #0 didn't work" << endl;
         return 1;
     }
 
-    
-    
-    menu.exibir();
-    resp = menu.lerResposta();
-    if(resp != 99 && resp !=67){
+    while(1)
+    {
+        menu.exibir();
+        resp = menu.lerResposta();
+        if(resp == 99 || resp == 67)
+        {
+            cout<< "Digite o seu usuário:" << endl;
+            cin >> nome;
+            menu.setUsuario(nome);
 
-        //break;
-
-    }
-    if(resp == 99 || resp == 67){
-        cout<< "Digite o nome do seu usuário:" << endl;
-        cin >> nome;
-        menu.setUsuario(nome);
-        
-
-        if( capture.isOpened() ) {
-            cout << "Video capturing has been started ..." << endl;
-
-            while (1)
+            if( capture.isOpened() ) 
             {
-                capture >> frame;
-                if( frame.empty() )
-                    break;
+                while (1)
+                {
+                    capture >> frame;
+                    if( frame.empty() )
+                        break;
 
-                t = detectAndDraw( frame, cascade, scale, tryflip );
-                cout << "Pontuacao: "<< pont << endl;
-                if(pont > pontos){
+                    t = detectAndDraw( frame, cascade, scale, tryflip );
+                    if(pont > pontos)
+                    {
+                        stream.open("records.txt", ios_base::out);
+                        novotexto = nome + " " + to_string(pont);
+                        stream << novotexto;
+                        stream.close();
+                    }
 
-                    stream.open("records.txt", ios_base::out);
-                    novotexto = nome + " " + to_string(pont);
-                    stream << novotexto;
-                    stream.close();
+                    if(t == 0)
+                    {
+                        cout << "VOCÊ PERDEU!" << endl << endl;
+                        char c = 27;
+                        break;
+                    }
+
+                    char c = (char)waitKey(10);
+                    if( c == 27 || c == 'q' || c == 'Q' )
+                        break;
                 }
-                if(t == 0)
-                    break;
-
-                char c = (char)waitKey(10);
-                if( c == 27 || c == 'q' || c == 'Q' )
-                    break;
             }
         }
-
+        else 
+            break;
     }
+    cout << endl << "ENCERRANDO..." << endl;
     return 0;
 }
 
-/**
- * @brief Draws a transparent image over a frame Mat.
- * 
- * @param frame the frame where the transparent image will be drawn
- * @param transp the Mat image with transparency, read from a PNG image, with the IMREAD_UNCHANGED flag
- * @param xPos x position of the frame image where the image will start.
- * @param yPos y position of the frame image where the image will start.
- */
 void drawTransparency(Mat frame, Mat transp, int xPos, int yPos) {
     Mat mask;
     vector<Mat> layers;
@@ -167,14 +159,6 @@ void drawTransparency(Mat frame, Mat transp, int xPos, int yPos) {
     transp.copyTo(frame.rowRange(yPos, yPos + transp.rows).colRange(xPos, xPos + transp.cols), mask);
 }
 
-/**
- * @brief Draws a transparent rect over a frame Mat.
- * 
- * @param frame the frame where the transparent image will be drawn
- * @param color the color of the rect
- * @param alpha transparence level. 0 is 100% transparent, 1 is opaque.
- * @param regin rect region where the should be positioned
- */
 void drawTransRect(Mat frame, Scalar color, double alpha, Rect region) {
     Mat roi = frame(region);
     Mat rectImg(roi.size(), CV_8UC3, color); 
@@ -184,6 +168,7 @@ void drawTransRect(Mat frame, Scalar color, double alpha, Rect region) {
 int detectAndDraw( Mat& img, CascadeClassifier& cascade, double scale, bool tryflip)
 {
     double t = 0;
+    bool lose = false;
     vector<Rect> faces;
     Mat gray, smallImg;
     Scalar color = Scalar(255,0,0);
@@ -199,12 +184,11 @@ int detectAndDraw( Mat& img, CascadeClassifier& cascade, double scale, bool tryf
 
     cascade.detectMultiScale( gray, faces,
         1.3, 2, 0
-        //|CASCADE_FIND_BIGGEST_OBJECT
-        //|CASCADE_DO_ROUGH_SEARCH
         |CASCADE_SCALE_IMAGE,
         Size(40, 40) );
     t = (double)getTickCount() - t;
     printf( "detection time = %g ms\n", t*1000/getTickFrequency());
+
 
     // Desenha uma imagem
     Mat pipe1 = cv::imread("../data/pipe_original_medio.png", IMREAD_UNCHANGED);
@@ -223,29 +207,23 @@ int detectAndDraw( Mat& img, CascadeClassifier& cascade, double scale, bool tryf
     Mat flappy = cv::imread("../data/flappy.png", IMREAD_UNCHANGED);
 
     c++;
-    cout << endl << "c = " << c << endl;
     
     if(c > 75)
     {
         x1-=2*velo;
         drawTransparency(smallImg, pipe2, x1, y_up);
-        printf("pipe::width: %d, height=%d\n", pipe2.cols, pipe2.rows);
-        drawTransparency(smallImg, pipe2, x1, y_up);
-        printf("pipe::width: %d, height=%d\n", pipe2.cols, pipe2.rows);
     }
 
     if(c > 150)
     {
         x2-=2*velo;
         drawTransparency(smallImg, pipe3, x2, y_down);
-        printf("pipe::width: %d, height=%d\n", pipe3.cols, pipe3.rows);
     }
 
     if(c > 225)
     {
         x3-=2*velo;
         drawTransparency(smallImg, pipe4, x3, y_up);
-        printf("pipe::width: %d, height=%d\n", pipe4.cols, pipe4.rows);
     }
 
     if(c % 100 == 0)
@@ -287,8 +265,14 @@ int detectAndDraw( Mat& img, CascadeClassifier& cascade, double scale, bool tryf
             color = Scalar(0,0,255);
             
             putText	(smallImg, "GAME OVER", Point(200, 200), FONT_HERSHEY_PLAIN, 3, Scalar(255,255,255));
-            //playlose();
+            playlose();
             //usleep(3*microsec);
+            c = 0;
+            float x = 600;
+            float x1 = 600;
+            float x2 = 600;
+            float x3 = 600;
+            return 0;
         }   
 
         else
@@ -298,17 +282,14 @@ int detectAndDraw( Mat& img, CascadeClassifier& cascade, double scale, bool tryf
 
         rectangle( smallImg, Point(cvRound(fac.x), cvRound(fac.y)), Point(cvRound((fac.x + fac.width)), cvRound((fac.y + fac.height))), color, 1);
 
-
         if(cvRound(r.x+(r.width/2) - 25) < 580 && cvRound(r.y+(r.width/2) - 25) < 580){
           drawTransparency(smallImg, flappy, cvRound(r.x+(r.width/2) - 25),cvRound(r.y+ (r.height/2) - 25));
         }
-
     }
 
     putText	(smallImg, to_string(pont), Point(320, 50), FONT_HERSHEY_PLAIN,3, Scalar(255,255,255)); // fonte
 
     imshow("result", smallImg );
-    printf("image::width: %d, height=%d\n", smallImg.cols, smallImg.rows );
 
     return 1;
 }
